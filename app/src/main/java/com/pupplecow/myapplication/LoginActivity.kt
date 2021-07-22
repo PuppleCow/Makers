1e55ce4a5973590f368f42606def40407d726c6e
이름 <>
2021-07-22 10:47:22 +0900

    로그인 수정

--- app/src/main/java/com/pupplecow/myapplication/LoginActivity.kt
+++ app/src/main/java/com/pupplecow/myapplication/LoginActivity.kt
@@ -1,13 +1,17 @@
 package com.pupplecow.myapplication
 
+import android.R.attr.button
 import android.content.DialogInterface
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View.OnTouchListener
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_login.view.*
+
 
 class LoginActivity : AppCompatActivity() {
     val login_data = arrayOf("선택", "상용직", "일용직")
@@ -26,6 +30,18 @@ class LoginActivity : AppCompatActivity() {
         //비밀번호 보여주기 이미지 클릭 시 text 보여주기 활성화(layout 분리해서)
 
 
+        /*비밀번호 숨기기 해제
+        login_imageButton.setOnClickListener(new OnTouchListener(){
+            private fun boolean onTouch(View v, MotionEvent event) {
+                login_imageButton.setOnTouchListener(OnTouchListener { v, event ->
+                    when (event.action) {
+                        MotionEvent.ACTION_UP -> editText.setInputType(InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD)
+                        MotionEvent.ACTION_DOWN -> editText.setInputType(InputType.TYPE_CLASS_TEXT)
+                    }
+                    true
+                })
+        }
+            */
         login_button.setOnClickListener {
 
 
@@ -33,7 +49,7 @@ class LoginActivity : AppCompatActivity() {
             val workpart = login_data[login_workpart.selectedItemPosition]
 
             //로그인 성공했을 경우(id와 password와 일용직/상용직 매치되는 정보 있으면)
-            if (login_editTextPhone.text.toString() == "01087347954" && login_editTextTextPassword.textView.toString() == "991109") {
+            if (login_editTextPhone.text.toString() == "01087347954" && login_editTextTextPassword.text.toString() == "991109") {
                 val intent = Intent(this@LoginActivity, HomeActivity1::class.java)
                 startActivity(intent)
 
@@ -60,12 +76,15 @@ class LoginActivity : AppCompatActivity() {
                 builder.show()
             }
             //상용/일용 선택되지 않았을 경우
+
             else if (workpart == "선택") {
                 val builder = AlertDialog.Builder(this)
                 builder.setMessage("상용직/일용직 중 선택해주세요")
                 builder.setPositiveButton("확인", null)
                 builder.show()
             }
+
+
             //휴대폰 or 비밀번호 틀렸을 경우
             else {
                 val builder = AlertDialog.Builder(this)
