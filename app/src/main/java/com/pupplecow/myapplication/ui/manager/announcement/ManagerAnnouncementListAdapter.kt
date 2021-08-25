0a458b495903fddd02bd78c903da1652d4f1f11d
jangnyer <jangnyer@naver.com>
2021-08-25 15:45:09 +0900

    관리자 공지사항

--- app/src/main/java/com/pupplecow/myapplication/ui/manager/announcement/ManagerAnnouncementListAdapter.kt
+++ app/src/main/java/com/pupplecow/myapplication/ui/manager/announcement/ManagerAnnouncementListAdapter.kt
@@ -11,10 +11,15 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.firestore.FirebaseFirestore
 import com.pupplecow.myapplication.R
 import com.pupplecow.myapplication.ui.manager.announcement.AnnouncementData
+import kotlinx.android.synthetic.main.activity_frameofannouncementlist.view.*
+import kotlinx.android.synthetic.main.manager_complaint_list_item.view.*
+import kotlinx.android.synthetic.main.manager_complaint_list_item.view.manager_complaint_list_title
 
 class ManagerAnnouncementListAdapter (val context: Context,val itemClick: (AnnouncementData) -> Unit):
     RecyclerView.Adapter<ManagerAnnouncementListAdapter.Holder>() {
-    var announcement: ArrayList<AnnouncementData> = arrayListOf()
+    var announcement: ArrayList<AnnouncementData> = arrayListOf(
+
+    )
     var firestore : FirebaseFirestore? = FirebaseFirestore.getInstance()
 
     init {
@@ -48,6 +53,10 @@ class ManagerAnnouncementListAdapter (val context: Context,val itemClick: (Annou
         //holder?.bind(announcement[position], context)
         var viewHolder = holder.itemView
 
+        viewHolder.frame_textView2.text=announcement[position].month
+        viewHolder.frame_textView6.text=announcement[position].date
+        viewHolder.frame_textView3.text=announcement[position].title
+
         holder?.itemView.setOnClickListener{
             itemClick(announcement[position])
         }
@@ -66,9 +75,9 @@ class ManagerAnnouncementListAdapter (val context: Context,val itemClick: (Annou
         //val EssentialRead = itemView?.findViewById<TextView>(R.id.frame_textView5)
 
         fun bind(Announce: AnnouncementData, context: Context) {
-            Month?.text = Announce.month
-            Title?.text = Announce.date
-            Category?.text = Announce.category
+//            Month?.text = Announce.month
+//            Title?.text = Announce.date
+//            Category?.text = Announce.category
             //EssentialRead?.text= Announce.EssentialRead
 
             //itemview를 클릭하면 itemClick(수행)
