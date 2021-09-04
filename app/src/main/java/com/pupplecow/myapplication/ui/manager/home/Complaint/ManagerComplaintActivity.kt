54db312312fbed6dfb0ff11784ba6880320f0036
jangnyer <jangnyer@naver.com>
2021-09-04 10:51:58 +0900

    ✨민원 디자인, 픽토그램 추가

--- app/src/main/java/com/pupplecow/myapplication/ui/manager/home/Complaint/ManagerComplaintActivity.kt
+++ app/src/main/java/com/pupplecow/myapplication/ui/manager/home/Complaint/ManagerComplaintActivity.kt
@@ -12,20 +12,20 @@ class ManagerComplaintActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_manager_complaint)
 
-        //서버에서 내용받아오기
+        //서버에서 제목,내용,날짜받아오기
         manager_MyComplaint_text_title.text="민원제목입니다."
         manager_MyComplaint_text_content.text="민원내용입니다."
 
 
 
-        //산업안전 뉴스 제목,링크 불러오기
-        manager_MyComplaint_text_news.text="뉴스 제목입니다."
-
-        manager_MyComplaint_text_news.setOnClickListener {
-            var intent =
-                Intent(Intent.ACTION_VIEW, Uri.parse("https://www.news1.kr/articles/?4386702"))
-            startActivity(intent)
-        }
+//        //산업안전 뉴스 제목,링크 불러오기
+//        manager_MyComplaint_text_news.text="뉴스 제목입니다."
+//
+//        manager_MyComplaint_text_news.setOnClickListener {
+//            var intent =
+//                Intent(Intent.ACTION_VIEW, Uri.parse("https://www.news1.kr/articles/?4386702"))
+//            startActivity(intent)
+//        }
 
         //목록버튼
         manager_MyComplaint_button_list.setOnClickListener {
